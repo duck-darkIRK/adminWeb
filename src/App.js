295eaa855ec319d0575e4727cb46a2621d2c1a27
AzenKain <doduykhanh344@gmail.com>
2024-02-19 12:55:52 +0700

    maintain 1.0

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const MainLayout = React.lazy(() => import('./layouts/MainLayout'))
 const Page404 = React.lazy(() => import('./views/Page404'))
 const Login = React.lazy(() => import('./views/Login.js'))
 const Privacy = React.lazy(() => import('./views/Privacy'))
+const ContactUs = React.lazy(() => import('./views/ContactUs'))
+
 class App extends React.Component {
 
   render(){
@@ -21,6 +23,7 @@ class App extends React.Component {
           <Routes>
             <Route exact path="/login" name="Login" element={<Login />} />
             <Route exact path='/privacy' element={<Privacy />} name='Privacy'/>
+            <Route exact path='/contact' element={<ContactUs />} name='ContactUs'/>
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route path="*" name="Home" element={<MainLayout />} />
           </Routes>
